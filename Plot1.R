344f0771f2ded13bf8226844430eba1364a3673f
Timo Grossenbacher <wnstnsmth@yahoo.com>
2015-03-04 22:37:07 +0100

    finished plot2, and modified plot1

--- Plot1.R
+++
@@ -1,3 +0,0 @@
-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "epc.zip", method = "curl")
-unzip("epc.zip")
-epc_sample <- scan('household_power_consumption.txt', what = 'list', nlines = 10, skip = 1, sep=";")
